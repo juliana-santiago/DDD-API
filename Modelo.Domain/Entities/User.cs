dd954626cfb7610d6ffed978f41bcc394e896d84
juliana-santiago <juju_msantiago@hotmail.com>
2021-02-09 19:47:28 -0300

    Estrutura DDD ja implementada no projeto

---
+++ Modelo.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo.Domain.Entities
+{
+    class User
+    {
+    }
+}
---
+++ Modelo.Service/Services/BaseService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo.Service.Services
+{
+    class BaseService
+    {
+    }
+}
